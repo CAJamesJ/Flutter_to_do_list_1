31483e66698033b51eee16f22ee62e26da601928
CAJamesJ <tall032123@gmail.com>
2024-05-17 19:04:13 -0400

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -15,24 +15,33 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(
+          // title: Center(
+          //   child: Text("MY APP BAR"),
+          // ),
+          title: Text("MY APP BAR"),
+          backgroundColor: Colors.deepPurple,
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.logout))
+          ],
+        ),
         body: Center(
           child: Container(
             height: 300,
             width: 300,
             decoration: BoxDecoration( //If you want further decorations, put color in decorations
-              color: Colors.green,
+              color: Colors.deepPurple,
               //curve the corners
               borderRadius: BorderRadius.circular(20),
             ),
             padding: EdgeInsets.all(25),
-            child: Text(
-              "James Jeon",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                ),
-            ),
+            child: Icon(
+              Icons.favorite,
+              color: Colors.white,
+              size: 64,
+              ),
           ),
         ),
       ),
