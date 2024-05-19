fc3d688b73e4a17b52b487fae6704f15e9ba51d5
CAJamesJ <tall032123@gmail.com>
2024-05-18 21:15:39 -0400

    part 2 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,33 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, must_be_immutable
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  // funcitons & methods
+  void userTapped() {
+    print("User tapped!");
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp( /* const is ignored in this project */
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.deepPurple[200],
-        appBar: AppBar(
-          // title: Center(
-          //   child: Text("MY APP BAR"),
-          // ),
-          title: Text("MY APP BAR"),
-          backgroundColor: Colors.deepPurple,
-          elevation: 0,
-          leading: Icon(Icons.menu),
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.logout))
-          ],
-        ),
         body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration( //If you want further decorations, put color in decorations
-              color: Colors.deepPurple,
-              //curve the corners
-              borderRadius: BorderRadius.circular(20),
+          child: GestureDetector(
+            onTap: userTapped,
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.deepPurple[200],
+              child: Center(child: Text("Tap me !"),)
             ),
-            padding: EdgeInsets.all(25),
-            child: Icon(
-              Icons.favorite,
-              color: Colors.white,
-              size: 64,
-              ),
           ),
         ),
       ),
