418f7e9d30dbd6b023363428a97421e750c5c3e6
CAJamesJ <tall032123@gmail.com>
2024-05-23 18:48:32 -0400

    TEXTFIELD

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,20 @@
-// ignore_for_file: prefer_const_constructors, must_be_immutable
-
 import 'package:flutter/material.dart';
-import 'package:to_do_list_1/pages/first_page.dart';
-import 'package:to_do_list_1/pages/home_page.dart';
-import 'package:to_do_list_1/pages/settings_page.dart';
+import 'pages/todo_page.dart';
 
 
 void main() {
   // WidgetsFlutterBinding.ensureInitialized(); /* This fixed expecting binder but got null error */
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( /* const is ignored in this project */
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstPage(), /*Used to be our Scaffold, but we can direct to the first page now */
-      routes: { /*This routes allow us to not having Navigator.push(); function in pages */
-        '/firstpage' : (context) => FirstPage(),
-        '/homepage' : (context) => HomePage(),
-        '/settingspage' : (context) => SettingsPage(),
-      },
+      home: ToDoPage(),
     );
   }
 }
